af266acfd6c081002334815cc2968e452099e6c6
mohamed elnemr <mohamed.elnemr@izam.co>
2021-08-27 05:38:51 +0200

    add comment to main function

--- bikeshare.py
+++ bikeshare.py
@@ -180,6 +180,7 @@ def viwe_row (df):
             break
         
 def main():
+    """ Start of the program """
     while True:
         city, month, day = get_filters()
         df = load_data(city, month, day)
